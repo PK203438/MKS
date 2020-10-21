98bc26a40cb59aaab4ed5e042424fc92022458ae
PK203438 <xklobu02@vutbr.cz>
2020-10-21 20:37:19 +0200

    Cv02 created. Task 2.1 completed.

---
+++ cv02/Debug/Src/main.d
@@ -0,0 +1,19 @@
+Src/main.o: ../Src/main.c \
+ C:/PK/gitmks/MKS/cv01/CMSIS/Device/ST/STM32F0xx/Include/stm32f0xx.h \
+ C:/PK/gitmks/MKS/cv01/CMSIS/Device/ST/STM32F0xx/Include/stm32f030x8.h \
+ C:/PK/gitmks/MKS/cv01/CMSIS/Include/core_cm0.h \
+ C:/PK/gitmks/MKS/cv01/CMSIS/Include/core_cmInstr.h \
+ C:/PK/gitmks/MKS/cv01/CMSIS/Include/core_cmFunc.h \
+ C:/PK/gitmks/MKS/cv01/CMSIS/Device/ST/STM32F0xx/Include/system_stm32f0xx.h
+
+C:/PK/gitmks/MKS/cv01/CMSIS/Device/ST/STM32F0xx/Include/stm32f0xx.h:
+
+C:/PK/gitmks/MKS/cv01/CMSIS/Device/ST/STM32F0xx/Include/stm32f030x8.h:
+
+C:/PK/gitmks/MKS/cv01/CMSIS/Include/core_cm0.h:
+
+C:/PK/gitmks/MKS/cv01/CMSIS/Include/core_cmInstr.h:
+
+C:/PK/gitmks/MKS/cv01/CMSIS/Include/core_cmFunc.h:
+
+C:/PK/gitmks/MKS/cv01/CMSIS/Device/ST/STM32F0xx/Include/system_stm32f0xx.h:
