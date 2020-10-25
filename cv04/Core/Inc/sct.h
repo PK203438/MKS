8ec4250b3299cd0cdadf18ad4deeef6b218426f8
PK203438 <xklobu02@vutbr.cz>
2020-10-25 20:10:14 +0100

    cv04 created. Task 2.2 completed.

---
+++ cv04/Core/Inc/sct.h
@@ -0,0 +1,15 @@
+/*
+ * sct.h
+ *
+ *  Created on: 24. 10. 2020
+ *      Author: 435736
+ */
+
+#ifndef SCT_H_
+#define SCT_H_
+
+void sct_led(uint32_t value);
+void sct_init(void);
+void sct_value(uint16_t value, uint8_t led);
+
+#endif /* SCT_H_ */
